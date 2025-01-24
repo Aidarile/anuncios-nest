14f0ea88524fb2811650d16b6cca0e3b1cd0d02b
Aida <aidarile@gmail.com>
2025-01-24 14:18:03 +0100

    auth controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 import * as bcryptjs from 'bcryptjs';
+import { CreateUsuarioDto } from 'src/usuarios/dto/create-usuario.dto';
+import { UsuariosService } from 'src/usuarios/usuarios.service';
 
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor( @Inject() private usuariosService: UsuariosService) {
+    }
+
+    @Post ("/singup")
+    async registrarNuevoUsuario ( @Body() createUsuarioDto : CreateUsuarioDto) {
+        return this.usuariosService.create(createUsuarioDto);
+    }
+}
